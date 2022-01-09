a0794167028153b6b285f6f948cd96d4b12c9444
atmygml <43545518+atmygml@users.noreply.github.com>
2022-01-09 21:07:05 +0800

    Create stateful_widget.dart

---
+++ stateful_widget.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class BoilerplateWidget extends StatefulWidget {
+  const BoilerplateWidget({Key? key}) : super(key: key);
+
+  @override
+  _BoilerplateWidgetState createState() => _BoilerplateWidgetState();
+}
+
+class _BoilerplateWidgetState extends State<BoilerplateWidget> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
